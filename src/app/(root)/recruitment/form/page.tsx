7c4926e08a4e53e9a71882869d1f51f8be24872e
misanthropic-codes <misanthropic.codes@outlook.com>
2025-02-18 14:13:17 +0530

    minor changes

--- src/app/(root)/recruitment/form/page.tsx
+++ src/app/(root)/recruitment/form/page.tsx
@@ -708,7 +708,7 @@ const Page = () => {
                     label="LinkedIn URL"
                     id="linkedIn"
                     type="text"
-                    placeholder="Share your linkedin profile link"
+                    placeholder="Enter Herre, Write NA if not"
                     handleChange={handleChange}
                     value={values.linkedIn}
                     error={errors.linkedIn}
@@ -719,7 +719,7 @@ const Page = () => {
                     label="Portfolio URL"
                     id="portfolio"
                     type="text"
-                    placeholder="Share your portfolio lin, if available"
+                    placeholder="SEnter Herre, Write NA if not"
                     handleChange={handleChange}
                     value={values.portfolio}
                     error={errors.portfolio}
@@ -730,7 +730,7 @@ const Page = () => {
                     label="Previous Clubs"
                     id="previousClubs"
                     type="text"
-                    placeholder="Enter the name of your current or previous tech club."
+                    placeholder="Enter Herre, Write NA if not"
                     handleChange={handleChange}
                     value={values.previousClubs}
                     error={errors.previousClubs}
