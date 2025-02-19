e6c809abb9b549e087f676fd66ab5cc823fb4ea4
deepak-raaaz <deepakjamui26@gmail.com>
2025-02-19 22:42:30 +0530

    placeholder edited

--- src/app/(root)/recruitment/form/page.tsx
+++ src/app/(root)/recruitment/form/page.tsx
@@ -707,7 +707,7 @@ const Page = () => {
                     label="LinkedIn URL"
                     id="linkedIn"
                     type="text"
-                    placeholder="Enter Herre, Write NA if not"
+                    placeholder="Enter URL(write NA if not applicable)"
                     handleChange={handleChange}
                     value={values.linkedIn}
                     error={errors.linkedIn}
@@ -718,7 +718,7 @@ const Page = () => {
                     label="Portfolio URL"
                     id="portfolio"
                     type="text"
-                    placeholder="SEnter Herre, Write NA if not"
+                    placeholder="Enter URL(write NA if not applicable)"
                     handleChange={handleChange}
                     value={values.portfolio}
                     error={errors.portfolio}
@@ -729,7 +729,7 @@ const Page = () => {
                     label="Previous Clubs"
                     id="previousClubs"
                     type="text"
-                    placeholder="Enter Herre, Write NA if not"
+                    placeholder="write NA if not applicable"
                     handleChange={handleChange}
                     value={values.previousClubs}
                     error={errors.previousClubs}
